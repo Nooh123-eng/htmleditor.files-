5072fdc451e88e6c0debcba8fc6c70a7d4b0dc5f
Noohbot <75690735+Nooh123-eng@users.noreply.github.com>
2021-04-03 10:31:31 -0400

    Add files via upload

---
+++ Htmleditor.js
@@ -0,0 +1,70 @@
+#code-display {
+    position:fixed;
+    width:250px;
+    height:270px;
+    top:80px;
+    left:300px;
+    overflow:auto;
+}
+#save-bt {
+    position:fixed;
+    left:450px;
+    top:360px;
+    background-color:maroon;
+    border:none;
+    color:white;
+    border-radius:15px;
+    padding:10px 25px;
+    display:block;
+}
+#save-bt:hover {
+    transition:0.4s;
+    background-color:rgb(255, 0, 0);
+}
+#update-bt {
+    position:fixed;
+    top:360px;
+    left:350px;
+    border:none;
+    background-color:green;
+    color:white;
+    border-radius:15px;
+    padding:10px 25px;
+    display:block;
+}
+#update-bt:hover {
+    transition:0.4s;
+    background-color:rgb(5, 255, 68);
+    color:black;
+}
+#back-bt {
+    position:fixed;
+    top:360px;
+    left:30px;
+    border:none;
+    background-color:darkblue;
+    color:white;
+    border-radius:15px;
+    padding:10px 25px;
+
+}
+#back-bt:hover {
+    transition:0.4s;
+    background-color:rgb(3, 184, 250);
+    color:black;
+}
+#view-div {
+    position:fixed;
+    z-index:2;
+    box-shadow:0 5px 5px grey;
+    display:none;
+    padding:1px;
+    top:0px;
+    left:0px;
+     background-color:white;
+     width:100%;
+     height:100%;
+}
+#view {
+
+}
